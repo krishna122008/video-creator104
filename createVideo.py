08fa7a0e7cf707c4fd4bb76287660e651be4e36a
krishna122008 <76689744+krishna122008@users.noreply.github.com>
2022-04-18 17:01:05 +0530

    Add files via upload

---
+++ createVideo.py
@@ -0,0 +1,36 @@
+import os
+from turtle import done
+import cv2
+
+
+path = "Images"
+
+images = []
+
+
+for file in os.listdir(path):
+    name, ext = os.path.splitext(file)
+
+    if ext in ['.gif', '.png', '.jpg', '.jpeg','.jfif']:
+        file_name = path+"/"+file
+
+        #print(file_name)
+               
+        images.append(file_name)
+        
+#print(len(images))
+count = len(images)
+frame = cv2.imread(images[0])
+height,width,channels = frame.shape
+size = (width,height)
+#print(images)
+#print(size)
+out = cv2.VideoWriter("project1.mp4",cv2.VideoWriter_fourcc(*"DIVX"),5,size)
+#FOR SUN RISE 
+#for i in range(count-1,0,-1):
+#for sun SET
+for i in range(0,count-1):
+    frame = cv2.imread(images[i])
+    out.write(frame)
+out.release()#releasing the video
+print("done")    
